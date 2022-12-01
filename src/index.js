fcd1f392fc40e168a7cb23d1b89e5657ab048566
Jeffrey Neal <jeffneal11@gmail.com>
2022-12-01 11:17:42 -0600

    added broken canvas

---
+++ src/index.js
@@ -0,0 +1,241 @@
+// let canvas,
+//   context,
+//   preX = 0,
+//   curX = 0,
+//   preY = 0,
+//   curY = 0,
+//   h = 0,
+//   w = 0,
+//   col = "black",
+//   stroke = 2,
+//   flag = false,
+//   dot_flag = false,
+//   rect;
+// //   mouseX = 0,
+// //   mouseY = 0;
+
+// function start() {
+//   canvas = document.getElementById("canvas");
+//   context = canvas.getContext("2d");
+//   rect = canvas.getBoundingClientRect();
+
+//   h = canvas.style.height;
+//   w = canvas.style.width;
+
+//   canvas.addEventListener(
+//     "mousemove",
+//     function (e) {
+//       findxy("move", e);
+//     },
+//     false
+//   );
+//   canvas.addEventListener(
+//     "mousedown",
+//     function (e) {
+//       findxy("down", e);
+//     },
+//     false
+//   );
+//   canvas.addEventListener(
+//     "mouseup",
+//     function (e) {
+//       findxy("up", e);
+//     },
+//     false
+//   );
+//   canvas.addEventListener(
+//     "mouseout",
+//     function (e) {
+//       findxy("out", e);
+//     },
+//     false
+//   );
+// }
+
+// function draw() {
+//   context.beginPath();
+//   context.moveTo(preX, preY);
+//   context.lineTo(curX, curY);
+//   context.strokeStyle = col;
+//   context.lineWidth = stroke;
+//   context.stroke();
+//   context.closePath();
+// }
+
+// function findxy(action, e) {
+//   if (action == "down") {
+//     preX = curX;
+//     preY = curY;
+//     curX = e.clientX - rect.left;
+//     curY = e.clientY - rect.top;
+//     console.log(curX, e.clientX, curY, e.clientY);
+//     //console.log(canvas.offsetLeft);
+//     //console.log(e.clientX, mouseX);
+
+//     flag = true;
+//     dot_flag = true;
+//     if (dot_flag) {
+//       context.beginPath();
+//       context.fillStyle = col;
+//       context.fillRect(curX, curY, 2, 2);
+//       context.closePath();
+//       dot_flag = false;
+//     }
+//   }
+//   if (action == "up" || action == "out") {
+//     flag = false;
+//   }
+//   if (action == "move") {
+//     if (flag) {
+//       preX = curX;
+//       preY = curY;
+//       curX = e.clientX - rect.left;
+//       curY = e.clientY - rect.top;
+//       draw();
+//     }
+//   }
+// }
+
+// function debug() {
+//   //console.log(mouseX);
+//   setTimeout(() => {
+//     debug();
+//   }, 100);
+// }
+
+// // onmousemove = function (e) {
+// //   mouseX = e.clientX;
+// //   mouseY = e.clientY; /*console.log(mouseX)*/
+// // };
+
+// start();
+// debug();
+
+let canv = document.getElementById("canvas"),
+  context = canvas.getContext("2d"),
+  col = "black",
+  stroke = 2,
+  flag = false,
+  dot_flag = false,
+  preX = 0,
+  preY = 0,
+  curX = 0,
+  curY = 0,
+  mouseX = 0,
+  mouseY = 0;
+
+let boundaries = {
+  left: canv.offsetLeft,
+  right: canv.offsetLeft + canv.offsetWidth,
+  top: canv.offsetTop,
+  bottom: canv.offsetTop + canv.offsetHeight,
+};
+
+// curX = canv.offsetLeft;
+// curY = canv.offsetTop;
+
+onmousemove = function (e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+};
+
+function start() {
+  //canvas = document.getElementById("canvas");
+
+  //rect = canvas.getBoundingClientRect();
+
+  canvas.addEventListener(
+    "mousemove",
+    function (e) {
+      findxy("move", e);
+    },
+    false
+  );
+  canvas.addEventListener(
+    "mousedown",
+    function (e) {
+      findxy("down", e);
+    },
+    false
+  );
+  canvas.addEventListener(
+    "mouseup",
+    function (e) {
+      findxy("up", e);
+    },
+    false
+  );
+  canvas.addEventListener(
+    "mouseout",
+    function (e) {
+      findxy("out", e);
+    },
+    false
+  );
+}
+
+function findxy(action, e) {
+  document.getElementById("debugger").style.left = `${curX}px`;
+  document.getElementById("debugger").style.top = `${curY}px`;
+
+  if (action == "down") {
+    preX = curX;
+    preY = curY;
+    if (
+      boundaries.left < mouseX &&
+      boundaries.right > mouseX &&
+      boundaries.top < mouseY &&
+      boundaries.bottom > mouseY
+    ) {
+      console.log("inside canvas");
+      curX = mouseX;
+      curY = mouseY;
+    }
+    //console.log(canvas.offsetLeft);
+    //console.log(e.clientX, mouseX);
+
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+      context.beginPath();
+      context.fillStyle = col;
+      context.fillRect(curX, curY, 2, 2);
+      context.closePath();
+      dot_flag = false;
+    }
+  }
+  if (action == "up" /* || action == "out"*/) {
+    flag = false;
+  }
+  if (action == "move") {
+    if (flag) {
+      preX = curX;
+      preY = curY;
+      if (
+        boundaries.left < mouseX &&
+        boundaries.right > mouseX &&
+        boundaries.top < mouseY &&
+        boundaries.bottom > mouseY
+      ) {
+        console.log("inside canvas");
+        curX = mouseX;
+        curY = mouseY;
+        document.getElementById("debugger").style.left = `${curX}px`;
+        document.getElementById("debugger").style.top = `${curY}px`;
+      }
+      draw();
+    }
+  }
+}
+
+function draw() {
+  context.beginPath();
+  context.moveTo(preX, preY);
+  context.lineTo(curX, curY);
+  context.strokeStyle = col;
+  context.lineWidth = stroke;
+  context.stroke();
+  context.closePath();
+}
+
+start();
